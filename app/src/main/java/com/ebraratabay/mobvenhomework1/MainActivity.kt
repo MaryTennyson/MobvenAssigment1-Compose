5f70e30591b40f519bdc1ac13ced6640efa6549e
MaryTennyson <ebrar-atabay@hotmail.com>
2024-05-06 23:40:43 +0300

    State Management

--- app/src/main/java/com/ebraratabay/mobvenhomework1/MainActivity.kt
+++ app/src/main/java/com/ebraratabay/mobvenhomework1/MainActivity.kt
@@ -9,23 +9,21 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Numbers
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Phone
-import androidx.compose.material.icons.filled.QuestionMark
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -33,7 +31,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ebraratabay.mobvenhomework1.models.User
@@ -46,16 +43,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MainActiviyScreen()
-            refreshData()
-
         }
 
     }
 }
-var usersList: MutableList<User> by mutableStateOf(mutableListOf())
+
 
 @Composable
 fun MainActiviyScreen() {
+    var usersList: MutableState<ArrayList<User>> = remember {
+        mutableStateOf(ArrayList())
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -64,7 +62,10 @@ fun MainActiviyScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         textField()
-        UserList()
+        UserList(usersList.value)
+        Button(onClick = { refreshData { usersList.value = it } }) {
+            Text(text = "Refresh Data")
+        }
 
     }
 }
@@ -113,38 +114,41 @@ fun textField() {
 
 
         Button(onClick = {
-            var user= User(filledName,filledSurname,filledPhoneNumber)
-            sendToDB(user) }) {
+            var user = User(filledName, filledSurname, filledPhoneNumber)
+            sendToDB(user)
+        }) {
             Text(text = "Save")
         }
     }
 
 
 }
-val db= Firebase.firestore
-fun sendToDB(user: User){
 
+val db = Firebase.firestore
+fun sendToDB(user: User) {
     db.collection("Users").add(user)
         .addOnSuccessListener { documentReference ->
             Log.d("SuccesFirebase", "DocumentSnapshot added with ID: ${documentReference.id}")
-            refreshData()
         }
         .addOnFailureListener { e ->
             Log.w("FailFirebase", "Error adding document", e)
         }
 
 }
-fun refreshData(){
+
+fun refreshData(updateList: (ArrayList<User>) -> Unit) {
+    var usersList = ArrayList<User>()
     db.collection("Users").get().addOnSuccessListener { result ->
         for (user in result) {
             val username = user["name"].toString()
             val usersurname = user["surname"].toString()
-            val phonenumber=user["phoneNumber"].toString()
-            val user= User(username, usersurname, phonenumber)
+            val phonenumber = user["phoneNumber"].toString()
+            val user = User(username, usersurname, phonenumber)
             println(user.name)
             println(user.surname)
             usersList.add(user)
         }
+        updateList(usersList)
     }
         .addOnFailureListener { exception ->
             Log.d(TAG, "Error getting documents: ", exception)
@@ -152,22 +156,26 @@ fun refreshData(){
 }
 
 @Composable
-fun UserList(){
-    refreshData()
-    LazyColumn( ){
-        items(items= usersList) { user ->
+fun UserList(usersList: ArrayList<User>) {
+    LazyColumn() {
+        items(items = usersList) { user ->
             ListItem(user = user)
         }
     }
 }
 
 @Composable
-fun ListItem(user:User){
-    Row(modifier= Modifier.fillMaxWidth().padding(top = 8.dp).border(1.dp, Color.Black),
-        horizontalArrangement = Arrangement.SpaceEvenly){
-        Text(text=user.name, Modifier.padding(vertical = 4.dp))
-        Text(text=user.surname, Modifier.padding(vertical = 4.dp))
-        Text(text=user.phoneNumber, Modifier.padding(vertical = 4.dp))
+fun ListItem(user: User) {
+    Row(
+        modifier = Modifier
+            .width(300.dp)
+            .padding(top = 8.dp)
+            .border(1.dp, Color.Black, CircleShape),
+        horizontalArrangement = Arrangement.Absolute.SpaceEvenly
+    ) {
+        Text(text = user.name, Modifier.padding(vertical = 16.dp))
+        Text(text = user.surname, Modifier.padding(vertical = 16.dp))
+        Text(text = user.phoneNumber, Modifier.padding(vertical = 16.dp))
     }
 
 }
